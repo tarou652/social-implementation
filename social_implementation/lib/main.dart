41a441566adb8163717bfa2a4adf2cc3dccfc3ec
tarou652 <rrrr20050307@gmail.com>
2023-09-29 09:41:41 +0900

    めも更新

--- social_implementation/lib/main.dart
+++ social_implementation/lib/main.dart
@@ -39,7 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("akakakasssssss"),
       ),
       body: Center(
         child: Column(
