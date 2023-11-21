21facddf305fd8c5dc5dbf318a6efa30807cc7e2
Your Name <you@example.com>
2023-11-21 14:51:10 +0900

    yuma
    timer

--- social_implementation/lib/main.dart
+++ social_implementation/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:SI/components/fotter.dart';
 import 'package:flutter/material.dart';
 import 'package:SI/components/header.dart';
+import 'dart:async';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(Myapp());
@@ -12,7 +14,7 @@ class Myapp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -27,48 +29,76 @@ class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
+class ClockTimer extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyHomePageState();
+  }
+}
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+
+  var _timeString = '00:00:00';
+
+  DateTime _startTime = DateTime.now();
+
+  var _timer;
+  var _isStart = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: Header(text: "時間指定"),
-      body: Container(
-        height: 400,
-        width: double.infinity,
-        color: Colors.blue,
-        child:Padding(
-          padding: EdgeInsets.all(20 ),
-        child: Column(
-          //mainAxisAlignment: MainAxisAlignment.center,//画面の真ん中に
-          children: <Widget>[
-            Text(
-              'You have pushed the buts:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-        )
-        
+      appBar: Header(text: "録音画面"),
+      backgroundColor: Color.fromRGBO(254, 246, 228, 1),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(_timeString, style: TextStyle(fontSize: 60)),
+          ),
+          Container(
+            width: 100,
+            height: 50,
+            color: Colors.greenAccent,
+            child: TextButton(
+                onPressed: _startTimer,
+                child: Text(_isStart ? 'STOP' : 'START')),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), 
-       bottomNavigationBar:Footer(currentIndex: 0,context: context,),
-      
+      bottomNavigationBar:Footer(currentIndex: 3,context: context),
     );
+  }
+  void _startTimer() {
+    setState(() {
+      _isStart = !_isStart;
+      if (_isStart) {
+        _startTime = DateTime.now();
+        _timer  = Timer.periodic(Duration(seconds: 1), _onTimer);
+      } else {
+        _timer.cancel();
+      }
+    });
+  }
 
+  void _onTimer(Timer timer) {
+    /// 現在時刻を取得
+    var now = DateTime.now();
+    /// 開始時刻と比較して差分を取得
+    var diff = now.difference(_startTime).inSeconds;
+
+    /// タイマーのロジック
+    var hour = (diff / (60 * 60)).floor();
+    var mod = diff % (60 * 60);
+    var minutes = (mod / 60).floor();
+    var second = mod % 60;
+
+    setState(() => {
+      _timeString = """${_convertTwoDigits(hour)}:${_convertTwoDigits(minutes)}:${_convertTwoDigits(second)}"""
+    });
+  }
+
+  String _convertTwoDigits(int number) {
+    return number >= 10 ? "$number" : "0$number";
   }
 }
