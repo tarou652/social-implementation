70e834829fcb976a8e61cd065e3c1e6ccfbc017d
tarou652 <rrrr20050307@gmail.com>
2024-01-01 19:07:34 +0900

    しゅうせい　ヒストリー

--- social_implementation/lib/history.dart
+++ social_implementation/lib/history.dart
@@ -1,7 +1,13 @@
 import 'package:SI/components/fotter.dart';
 import 'package:flutter/material.dart';
 import 'package:SI/components/header.dart';
-
+import 'dart:async';
+import 'package:intl/intl.dart';
+import 'package:record/record.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+import 'dart:io';
 void main() {
   runApp(HistoryPage());
 }
@@ -28,46 +34,153 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  void _incrementCounter() {
+  bool _recordingStatus = false; // 録音状態(true:録音中/false:停止中)
+  bool _playingStatus = false; // 再生状態(true:再生中/false:停止中)
+  Map<String, bool> _playingStatusMap = {};
+  AudioPlayer audioPlayer = AudioPlayer();
+  Directory appDocDir =Directory('');
+  List<FileSystemEntity> files = List<FileSystemEntity>.empty(growable: true);
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _Setfiles();
+  }
+  void _deleteFile(String filename) async {
+    // Get the index of the file in the list
+    int index = files.indexWhere((file) => basename(file.path) == filename);
+
+    // Remove the file from the list
+    files.removeAt(index);
+
+    // Remove the file from the file system
+    final file = File('${appDocDir.path}/recording/$filename');
+    if (await file.exists()) {
+      await file.delete();
+      print("消したよ");
+    }
+
+    // Remove the entry from the map
+    _playingStatusMap.remove(filename);
+
+    // Update the state to reflect the changes
+    setState(() {});
+  }
+  void _Setfiles() async {
+
+    appDocDir = await getApplicationDocumentsDirectory();
+    final recordingFolderPath = '${appDocDir.path}/recording';
+    final recordingDirectory = Directory(recordingFolderPath);
+    files = recordingDirectory.listSync();
+    // 各ファイル名に対してマップにエントリーを追加
+    for (var file in files) {
+      String fileName = basename(file.path);
+      _playingStatusMap[fileName] = false;
+    }
+    print("履歴画面${files.length}");
+    // ウィジェットを再描画
+    setState(() {});
+  }
+  void _startPlaying(String filename) async {
+    // 再生するファイルを指定
+    final directory = await getApplicationDocumentsDirectory();
+    String pathToWrite = directory.path;
+    final localFile = '$pathToWrite/recording/$filename';
+
+    // 再生開始
+    await audioPlayer.play(DeviceFileSource(localFile));
+
+    // 再生終了後、ステータス変更
+    audioPlayer.onPlayerComplete.listen((event) {
+      setState(() {
+        _playingStatus = false;
+      });
+    });
+  }
+
+  // 再生一時停止
+  void _pausePlaying() async {
+    await audioPlayer.pause();
+  }
+  // 再生の開始停止
+  void _playingHandle(String filename) {
+    print(filename);
     setState(() {
-      _counter++;
+      // 録音中の場合は録音停止
+      if (_recordingStatus) {
+        _recordingStatus = false;
+      }
+      // 再生中の場合は停止、停止中の場合は再生
+      _playingStatusMap[filename] = !_playingStatusMap[filename]!;
+
+      if (_playingStatusMap[filename]!) {
+        _startPlaying(filename);
+      } else {
+        _pausePlaying();
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: Header(text: "履歴"),
-      body: Container(
-        height: 400,
-        width: double.infinity,
-        color: Colors.blue,
-        child:Padding(
-          padding: EdgeInsets.all(20 ),
-        child: Column(
-          //mainAxisAlignment: MainAxisAlignment.center,//画面の真ん中に
-          children: <Widget>[
-            Text(
-              'You have pushed the buts:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-        )
-        
+      appBar: Header(text: "録音履歴"),
+      backgroundColor: Color.fromRGBO(254, 246, 228, 1),
+      body: Column(
+        children: [
+          Expanded(
+              child: Container(
+                child: ListView.builder(
+                  itemCount: files.length,
+                  itemBuilder: (context, index) {
+                    String fileName = basename(files[index].path);
+                    return Container(
+                      margin: EdgeInsets.symmetric(vertical: 5.0,horizontal: 20.0),
+                      color: Colors.lightBlue[50],
+                      child: Column(
+                        children: [
+                          ListTile(
+                            title: Text(fileName),
+                            tileColor: Colors.blue[50],
+                            trailing: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                ElevatedButton(
+                                  onPressed: () {_playingHandle(fileName);},
+                                  style: ElevatedButton.styleFrom(
+                                    shape: const CircleBorder(),
+                                    primary: Colors.lightBlue,
+                                  ),
+                                  child: _playingStatusMap[fileName] ?? false ? const Icon(Icons.stop) : const Icon(Icons.play_arrow),
+                                ),
+                                ElevatedButton(
+                                  onPressed: () {_deleteFile(fileName);},
+                                  style: ElevatedButton.styleFrom(
+                                    shape: const CircleBorder(),
+                                    primary: Colors.lightBlue,
+                                  ),
+                                  child: const Icon(Icons.delete),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Divider(
+                            height: 1,
+                            color: Colors.grey,
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              )
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), 
-       bottomNavigationBar:Footer(currentIndex: 2,context: context),
-      
+
+      bottomNavigationBar:Footer(currentIndex: 2,context: context),
+
     );
 
   }
